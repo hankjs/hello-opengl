c2dbcfe60766553818925f9d3a07f55a6dd237c5
hank <hank-c@qq.com>
2022-12-17 21:08:36 +0800

    feat: 使用vao作为渲染顶点依据

--- OpenGL-Sandbox/src/Application.cpp
+++ OpenGL-Sandbox/src/Application.cpp
@@ -116,6 +116,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -157,9 +161,13 @@ int main(void)
         2, 3, 0
     };
 
+    unsigned int vao;                   /* 保存顶点数组对象ID */
+    GLCall(glGenVertexArrays(1, &vao)); /* 生存顶点数组 */
+    GLCall(glBindVertexArray(vao));     /* 绑定顶点数组 */
+
     unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer)); /* 生成缓冲区 */
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); /* 绑定缓冲区 */
+    GLCall(glGenBuffers(1, &buffer));                                                        /* 生成缓冲区 */
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));                                           /* 绑定缓冲区 */
     GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW)); /* 设置缓冲区数据 */
 
     GLCall(glEnableVertexAttribArray(0)); /* 激活顶点属性-索引0-位置 */
@@ -180,6 +188,12 @@ int main(void)
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f)); /* 设置对应的统一变量 */
 
+    /* 解绑 */
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -187,7 +201,15 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        GLCall(glBindVertexArray(vao));
+
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+        GLCall(glEnableVertexAttribArray(0));
+        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
         /* 绘制 */
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
