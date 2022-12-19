83f85bc104854c8592175c1ed798d7c9ffe584d7
hank <hank-c@qq.com>
2022-12-19 23:15:36 +0800

    feat: 添加测试菜单

--- OpenGL-Sandbox/src/Application.cpp
+++ OpenGL-Sandbox/src/Application.cpp
@@ -89,20 +89,32 @@ int main(void)
         const char* glsl_version = "#version 330";
         ImGui_ImplOpenGL3_Init(glsl_version);
 
-        test::TestClearColor test;
+        test::Test *currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
+        
+        testMenu->RegisterTest<test::TestClearColor>("ClearColor");
 
         while (!glfwWindowShouldClose(window)) {
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
             renderer.Clear();
             
-            test.OnUpdate(0.0f);
-            test.OnRender();
-
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
+            if (currentTest)
             {
-                test.OnImGuiRender();
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("ImGui-Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
             }
 
             ImGui::Render();
@@ -110,6 +122,12 @@ int main(void)
 
             glfwSwapBuffers(window);
             glfwPollEvents();
+        }
+
+        delete currentTest;
+        if (currentTest != testMenu)
+        {
+            delete testMenu;
         }
     }
 
